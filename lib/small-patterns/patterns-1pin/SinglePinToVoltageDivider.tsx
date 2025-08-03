076dc30f86bf32d4f2fb90cc9c086111015f0357
seveibar <seveibar@gmail.com>
2025-08-02 18:01:07 -0700

    voltage divider slideVariations implementation

--- lib/small-patterns/patterns-1pin/SinglePinToVoltageDivider.tsx
+++ lib/small-patterns/patterns-1pin/SinglePinToVoltageDivider.tsx
@@ -2,17 +2,22 @@ import { getPinPosition } from "../utils/getPinPosition"
 import type { SmallPatternProps } from "../types"
 
 export const SinglePinToVoltageDivider = (props: SmallPatternProps) => {
-  const { pins, pinCount } = props
+  const { pins, pinCount, slideVariations } = props
   const pin = pins[0]!
   const pinPosition = getPinPosition(pin, pinCount)
+
+  const centerOffset = {
+    x: slideVariations[0] * 0.2,
+    y: slideVariations[1] * 0.2,
+  }
 
   return (
     <group>
       <resistor
         name={`R${pin}_1`}
         resistance={1000}
-        schX={pinPosition.x + 1}
-        schY={pinPosition.y + 1}
+        schX={pinPosition.x + 1 + centerOffset.x}
+        schY={pinPosition.y + 0.8 + centerOffset.y}
         schRotation="-90deg"
         connections={{
           pin1: `net.VCC`,
@@ -22,8 +27,8 @@ export const SinglePinToVoltageDivider = (props: SmallPatternProps) => {
       <resistor
         name={`R${pin}_2`}
         resistance={1000}
-        schX={pinPosition.x + 1}
-        schY={pinPosition.y - 1}
+        schX={pinPosition.x + 1 + centerOffset.x}
+        schY={pinPosition.y - 0.8 + centerOffset.y}
         schRotation="-90deg"
         connections={{
           pin1: `U1.${pin}`,
